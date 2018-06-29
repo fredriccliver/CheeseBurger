402a09492de46ad731087803dc94acf22134140f
fred J <fredriccliver@gmail.com>
2018-06-29 16:17:31 +0900

    zz

--- Class/CheeseBurger.py
+++ Class/CheeseBurger.py
@@ -17,8 +17,17 @@ class Classifier:
             return "Class 2" 
         else:
             return "Class 3"
-        
+    
+    fwVector = [ ,,, ]
 
+    burgerMatrix = [
+        [],[],[], ...
+    ]
+
+    recipe = [
+        [],[],[],...
+    ]
+    
     # data frame 을 받아서 모델을 생성.
     # 모델의 구체적인 형태는? data frame? matrix?
     def learn(self):
@@ -38,18 +47,65 @@ class Classifier:
 
     # 행렬을 받아서 그 행렬에 모델을 적용하고, 정답 배열을 return.
     # return 되는 것도 행렬. [index, class] 로 return.
-    def predict(self):
-        return
+    def predict(disp={"class", "class-prob", "probabilities"}):
+
+        pointArray = numpy.matmul(fwVector,burgerMatrix)
+        # result = [ ,,, ] elements count : counts of classes. 
+        
+        probabiliityArray = [
+            pointArray[0] / numpy.sum(pointArray),
+            pointArray[1] / numpy.sum(pointArray),
+            pointArray[2] / numpy.sum(pointArray),
+            ...
+        ]
+
+        # bestProb = [ className, Probability ]
+        bestProb = [ ,, ]
+
+        if disp=="class":           return bestProb[0]
+        elif disp=="class-prob":    return bestProb
+        elif disp=="probabilities"  return probabiliityArray
+        else                        return
+
+        
 
 class Appetizer:
-    recipe = [
-        [],[],[]
+
+    featureDictionary = [
+        0: "First feature name",
+        1: "Secont feature name",
+        2: "Third feature name",
+        ...
     ]
 
-    def scailing:
+    # Store min, max value for every features before scailing.
+    scalingMetaData = [
+        [min, max]
+        [min, max]
+    ]
+
+    # if want to know original value diversity of 'feature_A', code is below
+    # scalingMetaDate[featureDictionary.getIdx("feature_A")]
+
+
+    levelingDictionary = []
+
+    def scaling:
     
     def fill:
 
-    def ready:
-
+    def simpleReady:
+        fill()
+        scaling()
+        leveling()
     
+    # Leveling should apply to continuous values.
+    # need developing other leveling algorithm for Discontinuous(Categorical) value.
+    def leveling(data=[], level = 10):
+        
+        # check if need leveling.
+        current_level = len(set(data))
+        if(current_level <= level):
+            return data
+        
+
