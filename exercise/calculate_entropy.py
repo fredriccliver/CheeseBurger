21b1491673c439b4f270354137e58aeaa4e5f8f8
fred J <fredriccliver@gmail.com>
2018-06-29 18:19:24 +0900

    nothing special

--- exercise/calculate_entropy.py
+++ exercise/calculate_entropy.py
@@ -13,7 +13,7 @@ train['SexAndPclass'] = train['Sex'] + train['Pclass'].astype("str")
 train['GenerationsBy10'] = numpy.ceil(train['Age']/10)
 train['GenerationsBy20'] = numpy.ceil(train['Age']/20)
 train['GenerationsBy50'] = numpy.ceil(train['Age']/50)
-train['GenerationsBy150'] = numpy.ceil(train['Age']/150)
+#train['GenerationsBy150'] = numpy.ceil(train['Age']/150)
 
 print(train.head())
 #print(train.ix[0:10,"AgeClass"])
@@ -35,6 +35,23 @@ def calEntropy(feature_name):
             }
         )
     )[0]
+# feature 의 leveling 에 따라 entropy 가 달라짐.
+# 그렇다고해서 weight 에 level 갯수를 곱해주니, Name 과 같은 의미없는 feature 의 가중치가 너무 높아짐.
+# Entropy :
+#        Survived : 0.6659119735267652
+#            Name : 6.792344427470811
+#           SibSp : 0.9278185183972898
+#           Parch : 0.7821038727533371
+#           Cabin : 4.897561648814724
+#          Pclass : 0.9976616191577423
+#             Sex : 0.6489276088957644
+#        Embarked : 0.7602918973432804
+#             Age : 4.045611490075091
+#            Fare : 4.881534445778674
+#      FamilySize : 1.2638367056021338
+#    SexAndPclass : 1.6370862937306083
+# GenerationsBy10 : 1.752125743672301
+# GenerationsBy20 : 1.095247737266915
 
 def printEntropy(feature_name):
     print( '%15s'%feature_name + " : " + str(
@@ -43,7 +60,7 @@ def printEntropy(feature_name):
     return
 
 def calculateFeatureWeight(feature_name):
-    return (1/calEntropy(feature_name)) * len(train.groupby(feature_name))
+    return (1/calEntropy(feature_name)) # * len(train.groupby(feature_name))
 
 def printFeatureWeight(feature_name):
     print( '%15s'%feature_name + " : " + str(
