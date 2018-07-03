8b76efab824c1d4ff1884989b92c2af26ee3e507
fred J <fredriccliver@gmail.com>
2018-07-03 18:04:03 +0900

    nothing special

--- exercise/custom_entropy.py
+++ exercise/custom_entropy.py
@@ -9,16 +9,21 @@ import scipy.stats
 
 train = pd.read_csv('../data/train.csv')
 
-'''
-train['Generation'] = math.floor(train['Age']) >> 안됨.
-train['Generation'] = numpy.floor(train['Age']) >> 됨.
-numpy 를 써야함.
 
-Scipy, Sympy, Matplotlib, Pandas, Numpy 는 따로 훑어봐야할 듯.
-'''
 
 # Age 와 Generation 의 분포정도(편향도) 가 같다고 표현해낼 수 있는 entropy 계산식을 만들어야 함.
 
+
+
+train.loc[
+    train['Sex'] == 'male',
+    'Sex_digit'
+] = 1
+
+train.loc[
+    train['Sex'] == 'female',
+    'Sex_digit'
+] = 0
 
 
 # entropy 계산.
@@ -29,14 +34,31 @@ train['Old'] = numpy.floor(train['Age']/50)
 train = train.dropna()
 
 print(
-    train.ix[:,['Age', 'Generation','Generation_by20' 'Old']]
+    train.ix[:,['Sex_digit', 'Age', 'Generation','Generation_by20' 'Old']]
 )
 
+print(scipy.stats.entropy(train['Sex_digit']))
+print(scipy.stats.entropy(train['Pclass']))
+print(scipy.stats.entropy(train['Parch']))
 print(scipy.stats.entropy(train['Age']))
 print(scipy.stats.entropy(train['Generation']))
 print(scipy.stats.entropy(train['Generation_by20']))
 print(scipy.stats.entropy(train['Old']))
 print(scipy.stats.entropy(train['Fare']))
+# 4.553876891600542     (Sex_digit)
+# 5.137922658766879     (Pclass)
+# 4.035678834169101     (Parch)
+# 5.100398923741883     (Age)
+# 5.052093558144925     (Generation, group by mod 10)
+# 4.951890442507133     (Generation_by20, group by mod 20)
+# 3.58351893845611      (Old, group by mod 50)
+# 4.8541666107143575    (Fare)
+#
+# age 를 10단위, 20단위로 묶어도 entropy 는 동일함. > 제대로 동작.
+# 하지만 feature 마다 값차이가 별로 없음. log 함수로 값을 뽑아서 그런지?
+# 이대로는 못씀.
+
+
 
 '''
 # print(scipy.stats.entropy(train['Sex']))
@@ -49,5 +71,5 @@ TypeError: can't multiply sequence by non-int of type 'float'
 '''
 
 
-
 # nan 값이 있는 row 도 drop 하지 않고, 값이 없음 역시 값으로 다루면 좋을 듯.
+
