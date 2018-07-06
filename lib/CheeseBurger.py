08326028c2d06315957c6731aece5bc95e933b86
cliver <fredriccliver@gmail.com>
2018-07-07 01:37:28 +0900

    has made get_feature method that can return feature weight array

--- lib/CheeseBurger.py
+++ lib/CheeseBurger.py
@@ -46,6 +46,104 @@ class Classifier:
         # element 객수는
         return
 
+    # 추후에는 weight 를 feature, class 마다 따로 구해야 하지 않은지.
+    # multi class classification 에서는 return 을 [ class1 weight, class2 weight, ...]
+    # 클래스마다 중요한 feture 가 다를 수 있음.
+    def get_feature_weight(self, train_data: list, label_col_name: str, feature_col_name: str):
+        '''
+        arguments
+            train_data: list, 
+            label_col_name: str, 
+            feature_col_name: str
+        '''
+        class_names = list(train_data[[label_col_name]].groupby(label_col_name).size().keys())
+        levels = list(train_data[[feature_col_name]].groupby(feature_col_name).size().keys())
+        
+        divided_col_by_classes = []
+        
+        # for level in levels:
+        #     divided_col_by_classes.append(
+        #         list(train_data.loc[train_data[feature_col_name] == level, [label_col_name,feature_col_name]].groupby(label_col_name).size())
+        #     )
+
+        for selected_class in class_names:
+            divided_col_by_classes.append(
+                list(train_data.loc[train_data[label_col_name] == selected_class, [label_col_name,feature_col_name]].groupby(feature_col_name).size())
+            )
+        
+        summary_entropy = 0
+
+        # print(class_names)
+        # print(levels)
+        # print(divided_col_by_classes)
+        for val_list_by_class in divided_col_by_classes:
+            # print(val_list_by_class)
+            # print(
+            #     Appetizer.entropy_from_list(
+            #         self,
+            #         val_list_by_class
+            #     )
+            # )
+            summary_entropy += Appetizer.entropy_from_list(
+                self,
+                val_list_by_class
+            )
+        #print([summary_entropy, len(levels)])
+        return summary_entropy / len(class_names)
+
+    def get_feature_weight_vector(self, train_data: list, label_col_name: str, feature_col_name: str):
+        '''
+        arguments
+            train_data: list, 
+            label_col_name: str, 
+            feature_col_name: str
+        '''
+        class_names = list(train_data[[label_col_name]].groupby(label_col_name).size().keys())
+        levels = list(train_data[[feature_col_name]].groupby(feature_col_name).size().keys())
+        
+        divided_col_by_classes = []
+        
+        # for level in levels:
+        #     divided_col_by_classes.append(
+        #         list(train_data.loc[train_data[feature_col_name] == level, [label_col_name,feature_col_name]].groupby(label_col_name).size())
+        #     )
+
+        for selected_class in class_names:
+            divided_col_by_classes.append(
+                list(train_data.loc[train_data[label_col_name] == selected_class, [label_col_name,feature_col_name]].groupby(feature_col_name).size())
+            )
+        
+        weight_vector = []
+
+        # print(class_names)
+        # print(levels)
+        # print(divided_col_by_classes)
+        for val_list_by_class in divided_col_by_classes:
+            # print(val_list_by_class)
+            # print(
+            #     Appetizer.entropy_from_list(
+            #         self,
+            #         val_list_by_class
+            #     )
+            # )
+            weight_vector.append(
+                Appetizer.entropy_from_list(
+                    self,
+                    val_list_by_class
+                )
+            )
+        #print([summary_entropy, len(levels)])
+        return weight_vector #/ len(class_names)
+    
+    def get_weight(self, train_data: list, label_col_name: str, feature_col_name: str, return_type = "scalar"):
+        '''
+        return_type : scalar(or 0, default) | vector (or 1)
+        '''
+        if(return_type == "vector" or return_type == 1):
+            return Classifier.get_feature_weight_vector(self, train_data, label_col_name, feature_col_name)
+        else:
+            return Classifier.get_feature_weight(self, train_data, label_col_name, feature_col_name)
+
 
 
     # 행렬을 받아서 그 행렬에 모델을 적용하고, 정답 배열을 return.
@@ -189,7 +287,7 @@ class Appetizer:
         for val in data:
             ent += (val / sum(data)) ** 2
         
-        return ent * len(data)
+        return ent #* len(data) #클래스별 합산 엔트로피 구할때 문제가 생겨서 len 곱하는 부분 제거함.
 
     levelingDictionary = []
 
